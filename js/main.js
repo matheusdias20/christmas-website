5b095f340b3e563f0ed859c34fb8d9d6e8376af6
Matheus Vieira <mvdias20@gmail.com>
2021-12-19 23:53:47 -0300

    First Commit and Mobile First

---
+++ js/main.js
@@ -0,0 +1,68 @@
+
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+
+
+// Countdown
+
+const days = document.getElementById("days")
+const hours = document.getElementById("hours")
+const minutes = document.getElementById("minutes")
+const seconds = document.getElementById("seconds")
+
+
+
+const currentYear = new Date().getFullYear();
+
+const merryChristmas = new Date(`Dec 25, 2021 00:00:00`);
+
+
+function updateCountdowntime() {
+    const currentTime = new Date();
+    const diff = merryChristmas - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+    
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+
+
+setInterval(updateCountdowntime, 1000);
